dab80ecf95ac03bc1146053217a87aefeafbca87
TobRojekt <tobrojekt2@gmail.com>
2024-11-05 10:49:14 +0100

    add TextNode and NodeTypes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, NodeType
+
+def main():
+    textnode = TextNode("test text", NodeType.ITALIC, "https://www.boot.dev")
+    print(textnode)
+
+main()
