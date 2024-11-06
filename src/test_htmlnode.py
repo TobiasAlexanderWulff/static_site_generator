9d4b2614efce712e0c627b704ce05e6ce6d6d16f
TobRojekt <tobrojekt2@gmail.com>
2024-11-06 11:46:10 +0100

    add unittests LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -22,4 +22,25 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_html3(self):
         node = HTMLNode()
-        self.assertRaises(Exception, node.props_to_html)
+        self.assertEqual(node.props_to_html(), "")
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "a test value")
+        self.assertEqual(node.to_html(), "<p>a test value</p>")
+
+    def test_to_html2(self):
+        props = {
+            "href": "https://www.google.com",
+        }
+        node = LeafNode("a", "a beautiful link", props=props)
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">a beautiful link</a>')
+
+    def test_to_html3(self):
+        props = {
+            "id": "special_link",
+            "href": "https://www.google.com",
+        }
+        node = LeafNode("a", "a beautiful link", props=props)
+        self.assertEqual(node.to_html(), '<a id="special_link" href="https://www.google.com">a beautiful link</a>')   
+    
