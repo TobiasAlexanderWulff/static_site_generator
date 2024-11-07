86d9bca090255cb515e969d89276d7b126bf7e15
TobRojekt <tobrojekt2@gmail.com>
2024-11-07 14:50:30 +0100

    add unittests test to textnodes

--- src/test_main.py
+++ src/test_main.py
@@ -328,3 +328,28 @@ class TestMain(unittest.TestCase):
             TextNode(".", TextType.LINK, url="https://www.test.com/page#5"),
         ]
         self.assertEqual(output_nodes, expected_output)
+
+
+    def test_text_to_textnodes(self):
+        input = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        output = text_to_textnodes(input)
+        expected_output = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(output, expected_output)
+
+
+    def test_text_to_textnodes2(self):
+        input = ""
+        output = text_to_textnodes(input)
+        expected_output = []
+        self.assertEqual(output, expected_output)
